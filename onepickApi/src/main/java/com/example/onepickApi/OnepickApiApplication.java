efc35cd72d3601389a5af2db0eda47a805646cfa
Jisun Kim <wltjs5963@naver.com>
2024-06-11 17:35:47 +0900

    서버 세팅

---
+++ onepickApi/src/main/java/com/example/onepickApi/OnepickApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.onepickApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnepickApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnepickApiApplication.class, args);
+	}
+
+}
---
+++ onepickApi/src/test/java/com/example/onepickApi/OnepickApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onepickApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnepickApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
