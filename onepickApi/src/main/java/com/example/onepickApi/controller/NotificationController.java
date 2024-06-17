73232ac913d065328ef22bc97325c0003756653c
Jisun Kim <wltjs5963@naver.com>
2024-06-17 09:03:44 +0900

    푸쉬알림 오류 주석처리

--- onepickApi/src/main/java/com/example/onepickApi/controller/NotificationController.java
+++ onepickApi/src/main/java/com/example/onepickApi/controller/NotificationController.java
@@ -84,7 +84,7 @@ public class NotificationController {
 		
 			String token = "eTIKMWTX2BKnNjgSSAOQGJ:APA91bEhKCwoPOqrq0uFr5N46nwM1JrGM5PYU9N4A8a3PwGV8mJfNlWyqmLGoN0dIT0MyS97soly_sSvgUkwQJ9M1SaVDbmvh5oBCxheisclJMtZ2DVbB6yZbA171sT2wwh6lzet_Bra"; // 실제로는 DB에서 가져옴
 			
-				notificationService.sendNotification(token, "번겁슙니다", "알림성공추카드려욥!");
+				//notificationService.sendNotification(token, "번겁슙니다", "알림성공추카드려욥!");
 				
 					
 		
--- onepickApi/src/main/java/com/example/onepickApi/service/NotificationService.java
+++ onepickApi/src/main/java/com/example/onepickApi/service/NotificationService.java
@@ -1,27 +1,28 @@
 package com.example.onepickApi.service;
 
-import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.Message;
-import com.google.firebase.messaging.Notification;
+import javax.management.Notification;
+
 import org.springframework.stereotype.Service;
+
+//import com.google.firebase.messaging.FirebaseMessaging;
 
 @Service
 public class NotificationService {
 
-    public void sendNotification(String token, String title, String body) {
-        Message message = Message.builder()
-                .setNotification(Notification.builder()
-                        .setTitle(title)
-                        .setBody(body)
-                        .build())
-                .setToken(token)
-                .build();
-
-        try {
-            String response = FirebaseMessaging.getInstance().send(message);
-            System.out.println("Successfully sent message: " + response);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+//    public void sendNotification(String token, String title, String body) {
+//        Message message = Message.builder()
+//                .setNotification(Notification.builder()
+//                        .setTitle(title)
+//                        .setBody(body)
+//                        .build())
+//                .setToken(token)
+//                .build();
+//
+//        try {
+//            String response = FirebaseMessaging.getInstance().send(message);
+//            System.out.println("Successfully sent message: " + response);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
