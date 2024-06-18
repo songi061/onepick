145513318793c03aba528f46e585cba98eeb166a
Jia Yoo <jiayoo.dev@gmail.com>
2024-06-18 09:51:41 +0900

    기업 마이페이지에서 해당기업에 지원한 지원자 리스트 뽑는 방법 변경

--- onepickApi/src/main/java/com/example/onepickApi/controller/ApplyController_jia.java
+++ onepickApi/src/main/java/com/example/onepickApi/controller/ApplyController_jia.java
@@ -1,5 +1,6 @@
 package com.example.onepickApi.controller;
 
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
 
@@ -12,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.onepickApi.entity.ApplyList;
+import com.example.onepickApi.entity.JobAd;
 import com.example.onepickApi.repository.ApplyListRepository;
+import com.example.onepickApi.repository.JobAdRepository;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -23,8 +26,10 @@ public class ApplyController_jia {
 
 	@Autowired
 	private ApplyListRepository applyListRepo;
+	@Autowired
+	private JobAdRepository jobadRepo;
 	
-	
+	//우리회사 지원한 지원자 리스트 뽑기
 	@GetMapping("/company")
 	public ResponseEntity<List<ApplyList>> getApplyListForComMyPage(HttpServletRequest request) {
 		Enumeration<String> headers = request.getHeaderNames();
@@ -33,7 +38,22 @@ public class ApplyController_jia {
 				System.out.println(request.getHeader("username"));
 			}
 		}
-		List<ApplyList> applyList = applyListRepo.findByCid(request.getHeader("username"));
+		List<JobAd> myJobAdList = jobadRepo.findByUsername(request.getHeader("username"));
+		System.out.println(myJobAdList);
+		List<Long> jnoList = new ArrayList<>();
+		List<ApplyList> applyList = new ArrayList<>();
+		for(JobAd ja : myJobAdList) {
+			Long jno = ja.getJno();
+			jnoList.add(jno);
+		}
+		System.out.println(jnoList);
+		for(Long jno : jnoList) {
+			ApplyList al = applyListRepo.findByJno(jno);
+			if(al != null) {
+				applyList.add(al);
+			}
+		}
+		System.out.println(applyList);
 			return new ResponseEntity<>(applyList, HttpStatus.OK);
 	}
 	
